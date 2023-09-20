94cbf40e4cdf4bc1a271cabfd267be71decca48f
sharp1 <cundiffterry20@gmail.com>
2023-09-20 08:00:27 -0400

    Initial commit

---
+++ Website/index.js
@@ -0,0 +1,18 @@
+<<<<<<< HEAD
+const counter = document.querySelector(".counter-number");
+async function updateCounter() {
+    let response = await fetch("https://5xvqx57wjroqv6yp32eucyueia0zvaka.lambda-url.us-east-1.on.aws/");
+    let data = await response.json();
+    counter.innerHTML = ` Views: ${data}`;
+}
+
+=======
+const counter = document.querySelector(".counter-number");
+async function updateCounter() {
+    let response = await fetch("https://5xvqx57wjroqv6yp32eucyueia0zvaka.lambda-url.us-east-1.on.aws/");
+    let data = await response.json();
+    counter.innerHTML = ` Views: ${data}`;
+}
+
+>>>>>>> a4514eb (Initial commit)
+updateCounter();
